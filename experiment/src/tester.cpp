e377f3c638f7517a676a24be58acc370167b56f0
Unknown <437998194@qq.com>
2017-11-20 23:10:10 -0800

    Add definitions for class Tester
    Tester::Analyze is to be continued.

---
+++ experiment/src/tester.cpp
@@ -0,0 +1,99 @@
+#include "tester.h"
+
+Tester::Tester() {}
+
+int Tester::RunTest(string scheduling_algorithm, string testcase_file, string result_file) {
+    cout << "Test on " << testcase_file << " begins..." << endl;
+    int read_status = ReadTestcase(testcase_file);
+    if (read_status == -1) {
+        cout << "Test failed: ReadTestCase error." << endl;
+        return -1;
+    }
+    vector<Allocation> allocations = GetAllocation(scheduling_algorithm);
+    if (allocations.size() < requests.size()) {
+        cout << "Test failed: GetAllocation error." << endl;
+        return -1;
+    }
+    int analyze_status = Analyze(allocations, scheduling_algorithm, testcase_file, result_file);
+    if (analyze_status == -1) {
+        cout << "Test failed: Analyze error." << endl;
+        return -1;
+    }
+    cout << "Test finished. Result in " << result_file << endl;
+    return 0;
+}
+
+int Tester::ReadTestcase(string testcase_file) {
+    traffic_pattern_table.clear();
+    utility_rate.clear();
+    requests.clear();
+    claimed_usage.clear();
+    actual_usage.clear();
+
+    ifstream testcase(testcase_file);
+    if (!testcase) {
+        cout << "Open file " << testcase_file << " error." << endl;
+        return -1;
+    }
+
+    testcase >> num_channel;
+    testcase >> RB_time >> RB_size >> RB_bandwidth;
+
+    testcase >> num_pattern;
+    string type;
+    int priority;
+    double bandwidth;
+    double delay;
+    double max_req_size;
+    double price_RB;
+    for (int i = 0; i < num_pattern; i++) {
+        testcase >> type >> priority >> bandwidth >> delay >> max_req_size >> price_RB;
+        traffic_pattern_table[type] = Pattern(priority, bandwidth, delay, max_req_size, price_RB);
+    }
+
+    testcase >> num_UE;
+    int id;
+    double rate;
+    for (int i = 0; i < num_UE; i++) {
+        testcase >> id >> rate;
+        utility_rate[id] = rate;
+    }
+
+    testcase >> num_request;
+    double time;
+    double size;
+    string claimed, actual;
+    for (int i = 0; i < num_request; i++) {
+        testcase >> time >> id >> claimed >> size >> actual;
+        size = min(traffic_pattern_table[claimed].max_req_size, size);
+        requests.push_back(Request(time, id, traffic_pattern_table[claimed].priority, traffic_pattern_table[claimed].bandwidth, traffic_pattern_table[claimed].delay, size));
+        claimed_usage.push_back(claimed);
+        actual_usage.push_back(actual);
+    }
+
+    return 0;
+}
+
+vector<Allocation> Tester::GetAllocation(string scheduling_algorithm) {
+    Scheduler* scheduler;
+    if (scheduling_algorithm == "null") scheduler = new SchedulerNull(num_channel, RB_time, RB_size, RB_bandwidth);
+    else scheduler = new Scheduler(num_channel, RB_time, RB_size, RB_bandwidth);
+    vector<Allocation> allocations = scheduler->GetAllocation(requests, utility_rate);
+    delete scheduler;
+    return allocations;
+}
+
+int Tester::Analyze(vector<Allocation>& allocations, string scheduling_algorithm, string testcase_file, string result_file) {
+    ofstream result(result_file);
+    if (!result) {
+        cout << "Open file " << result_file << " error." << endl;
+        return -1;
+    }
+    
+    result << "Scheduling Algorthm: " << scheduling_algorithm << endl;
+    result << "Testcase: " << testcase_file << endl;
+    
+    // To be continued
+
+    return 0;
+}
